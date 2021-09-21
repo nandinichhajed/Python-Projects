da30d9ea868046afbda4b6bdbe72ba5572b1a257
nandinichhajed <nandinichhajed08@gmail.com>
2021-09-21 15:16:25 +0530

    Image fto skectch

---
+++ Image Editing/image to sketch.py
@@ -0,0 +1,20 @@
+import numpy as np
+import cv2
+
+# Reading image
+img = cv2.imread(r"File path")
+
+# Edges
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray = cv2.medianBlur(gray, 5)
+edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9)
+
+# cartoonization
+color = cv2.bilateralFilter(img, 9, 250, 250)
+cartoon = cv2.bitwise_and(color, color, mask =  edges)
+
+cv2.imshow("Image", img)
+cv2.imshow("edges", edges)
+cv2.imshow("Cartoon", cartoon)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
